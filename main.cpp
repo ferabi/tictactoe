13d3e8f7774f8ff07eca01ec63d4cfc46ac836f6
Abhishek Kumar <abhishek.k.1123@gmail.com>
2022-03-15 14:19:49 +0530

    Test on VSCode

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "grid.h"
-
+#include <iostream>
 int main()
 {
     Grid grid{};
     grid.print();
     grid.reset();
     grid.print();
+    std::cout << "This is a test\n";
     return 0;
 };
